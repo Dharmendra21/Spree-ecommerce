8a692279b8a97f43a932f7f54a84e098b632989d
dharm21 <you@example.com>
2018-07-26 19:41:14 +0530

    Initial commit

---
+++ app/models/spree/image_decorator.rb
@@ -0,0 +1,12 @@
+
+  # Spree::Image.attachment_definitions[:attachment][:styles] = {
+  #   :mini => '48x48>', # thumbs under image
+  #   :small => '100x100>', # images on category view
+  #   :product => '240x240>', # full product image
+  #   :large => '600x600>', # light box image
+  #   :popup => '600x600>',
+  #   :crossselling => '80x80>',
+  #   :articlepreview => '175x175>',
+  #   :info => '260x260>'}
+  # attachment_definitions[:attachment][:url] = "/media/images/:style/:basename.:extension"
+  # attachment_definitions[:attachment][:path] = ":rails_root/public/media/images/:style/:basename.:extension"
